739372872142ccee55c1ac5bf6304975579623dd
Leandro Souza <48530574+Lsouz44@users.noreply.github.com>
2023-04-10 16:26:05 -0300

    Casa 2D - Atividade finalizada

--- casa.cpp
+++ casa.cpp
@@ -1,8 +1,39 @@
-#include<GL/glut.h>
+#include <stdlib.h>
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <GL/glut.h>
+#include <math.h>
+
+
+float grausParaRadianos(float angulo_graus) {
+    return (angulo_graus * 2 * M_PI)/360.0;
+}
+
+void desenharCirculo(float raio, float x0, float y0, int definicao, float vermelho, float verde, float azul) {
+    int i;
+    float passo = grausParaRadianos(360.0/definicao);
+    float angulo = passo;
+    float x = 0.0, y = 0.0;
+
+    //modo de pintura do objeto
+    glShadeModel(GL_FLAT);
+
+    glBegin(GL_POLYGON);
+        glColor3f(vermelho, verde, azul);
+        for (i = 0; i < definicao; i++) {
+            x = raio * cos(angulo) + x0;
+            y = raio * sin(angulo) + y0;
+
+            glVertex2f(x, y);
+
+            angulo += passo;
+        }
+    glEnd();
+}
 
 void init(){
 	//display cor da janela
-	glClearColor(0.5, 0.9, 0.4, 0.0);
+	glClearColor(1.0, 1.0, 1.0, 0.0);
 	//parâmetro de projeção
 	glMatrixMode(GL_PROJECTION);
 	//transformação 2d 
@@ -15,7 +46,7 @@ void home(){
 	glClear(GL_COLOR_BUFFER_BIT);
 	
 	//definir a cor do telhado
-	glColor3f(0.3, 0.5, 0.8);
+	glColor3f(0.9, 0.6, 0.1);
 	glBegin(GL_POLYGON);	   
 	glVertex2i(200, 500);
 	glVertex2i(600, 500);
@@ -24,7 +55,7 @@ void home(){
 	glEnd();
 	
 	// Parte superior do telhado frontal
-	glColor3f(0.5, 0.2, 0.0);
+	glColor3f(0.8, 0.4, 0.1);
 	glBegin(GL_TRIANGLES);	
 	glVertex2i(200, 500);
 	glVertex2i(100, 350);
@@ -33,7 +64,7 @@ void home(){
 	
 	//parede frontal
 	       // ( R,  G ,  B)
-	glColor3f(0.04, 0.1, 0.2);
+	glColor3f(0.9, 0.8, 0.2);
 	glBegin(GL_POLYGON);
 		    //( X , Y)
 	glVertex2i(100, 350);
@@ -43,7 +74,7 @@ void home(){
 	glEnd();
 	
 	//parede lateral
-	glColor3f(0.1, 0.2, 0.3);
+	glColor3f(0.9, 0.9, 0.3);
 	glBegin(GL_POLYGON);
 		   // ( X , Y)
 	glVertex2i(300,350);
@@ -63,57 +94,48 @@ void home(){
 	glEnd();
 	
 	//maçaneta da porta da frente
-        glColor3f(0.3, 0.7, 0.9);
-        glPointSize(15);
-        glBegin(GL_POINTS);
-        glVertex2i(170, 170);
-        glEnd();
+	desenharCirculo(10.0, 170, 170, 30, 0.8, 0.3, 0.1);
+	glEnd();
+
+	//janela frontal
+	desenharCirculo(35.0, 200, 400, 30, 0.3, 0.3, 0.8);
+	glEnd();
 	
 	 //janela 1
-    	glColor3f(0.8, 0.8, 0.1);
+    	glColor3f(0.3, 0.3, 0.8);
    	glBegin(GL_POLYGON);
     	glVertex2i(330, 320);
     	glVertex2i(450, 320);
     	glVertex2i(450, 230);
     	glVertex2i(330, 230);
     	glEnd();
-    	//linha da janela 1
-    	glColor3f(0.1, 0.1, 0.1);
-    	glLineWidth(5);
-    	glBegin(GL_LINES);
-    	glVertex2i(390, 320);
-    	glVertex2i(390, 230);
-    	glVertex2i(330, 273);
-    	glVertex2i(450, 273);
-    	glEnd();
+    	
     	//janela 2
-    	glColor3f(0.8, 0.8, 0.1);
+    	glColor3f(0.3, 0.3, 0.8);
     	glBegin(GL_POLYGON);
     	glVertex2i(530, 320);
     	glVertex2i(650, 320);
     	glVertex2i(650, 230);
     	glVertex2i(530, 230);
     	glEnd();
-    	//linha da janela 2
-    	glColor3f(0.1, 0.1, 0.1);
-    	glLineWidth(5);
-    	glBegin(GL_LINES);
-    	glVertex2i(590, 320);
-    	glVertex2i(590, 230);
-    	glVertex2i(530, 273);
-    	glVertex2i(650, 273);
-    	glEnd();
-
-    	//caminho de entrada
-    	glColor3f(0.3, 0.5, 0.7);
-    	glLineWidth(3);
-    	glBegin(GL_POLYGON);
-    	glVertex2i(150, 100);
-    	glVertex2i(250, 100);
-    	glVertex2i(210, 0);
-    	glVertex2i(40, 0);
-    	glEnd();
 	
+	//chaminé
+    	glColor3f(0.5, 0.2, 0.0);
+    	glBegin(GL_POLYGON);
+ 	glVertex2i(375, 530);
+    	glVertex2i(425, 530);
+    	glVertex2i(425, 500);
+    	glVertex2i(375, 500);
+    	glEnd();
+    	
+	//chaminé topo
+    	glColor3f(0.5, 0.2, 0.0);
+    	glBegin(GL_POLYGON);
+	glVertex2i(435, 550);
+    	glVertex2i(365, 550);
+ 	glVertex2i(365, 530);
+    	glVertex2i(435, 530);
+    	glEnd();
 	
 	glFlush();
 }
@@ -130,16 +152,17 @@ int main(int argc, char ** argv){
 	
 	glutCreateWindow("Casa 2D");
 	
+	glutDisplayFunc(home);
 	
 	init();
-	glutDisplayFunc(home);
+	
 	
 	glutMainLoop();
 
 }
 
-// para compilar no ubuntu :
-// gcc house.c -lglut -lGL -lGLU -lm
+// para compilar:
+// g++ casa.cpp -o casa -lGL -lGLU -lglut -lm
 
-// para executar :
-// ./a.out
+// para executar:
+// ./casa
