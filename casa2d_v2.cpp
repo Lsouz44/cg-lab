be6a711f18c62659aa92adba3c9b840a3f2839f3
Leandro Souza <48530574+Lsouz44@users.noreply.github.com>
2023-05-21 19:04:25 -0300

    Update casa2d_v2.cpp

--- casa2d_v2.cpp
+++ casa2d_v2.cpp
@@ -95,7 +95,7 @@ int main(int argc, char** argv)
     
     return 0;
 }
-*/
+
 
 #include <GL/glut.h>
 
@@ -133,6 +133,79 @@ void display()
     glPushMatrix();
     glTranslatef(-0.3, 0.0, 0.0); // Mover a janela para a esquerda
     glScalef(0.3, 0.3, 1.0); // Reduzir o tamanho da janela
+    glRectf(-0.5, -0.5, 0.5, 0.5);
+    glPopMatrix();
+    
+    glFlush();
+}
+
+int main(int argc, char** argv)
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(600, 600);
+    glutCreateWindow("Casa 2D");
+    glClearColor(1.0, 1.0, 1.0, 1.0);
+    gluOrtho2D(-2.0, 2.0, -2.0, 2.0);
+    glutDisplayFunc(display);
+    glutMainLoop();
+    
+    return 0;
+}
+*/
+
+#include <GL/glut.h>
+
+void display()
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    
+    // Desenhar a base da casa
+    glColor3f(0.8, 0.8, 0.8); // Cor cinza claro
+    glPushMatrix();
+    glTranslatef(0.0, -0.3, 0.0);
+    glScalef(1.5, 0.8, 1.0); // Ampliar a base horizontalmente
+    glRectf(-0.5, -0.5, 0.5, 0.5);
+    glPopMatrix();
+    
+    // Desenhar o telhado
+    glColor3f(0.8, 0.4, 0.2); // Cor marrom claro
+    glPushMatrix();
+    glTranslatef(0.0, 0.3, 0.0); // Mover o telhado para cima
+    glScalef(1.5, 0.8, 1.0); // Ampliar o telhado horizontalmente
+    glRotatef(45.0, 0.0, 0.0, 1.0); // Girar o telhado em 45 graus
+    glRectf(-0.5, -0.5, 0.5, 0.5);
+    glPopMatrix();
+    
+    // Desenhar a porta
+    glColor3f(0.4, 0.2, 0.0); // Cor marrom escuro
+    glPushMatrix();
+    glTranslatef(-0.15, -0.4, 0.0); // Mover a porta para baixo e para a esquerda
+    glScalef(0.3, 0.4, 1.0); // Reduzir a altura e a largura da porta
+    glRectf(-0.5, -0.5, 0.5, 0.5);
+    glPopMatrix();
+    
+    // Desenhar a maçaneta
+    glColor3f(0.0, 0.0, 0.0); // Cor preta
+    glPushMatrix();
+    glTranslatef(-0.05, -0.4, 0.0); // Mover a maçaneta para baixo e para a esquerda
+    glScalef(0.1, 0.1, 1.0); // Reduzir o tamanho da maçaneta
+    glRectf(-0.5, -0.5, 0.5, 0.5);
+    glPopMatrix();
+    
+    // Desenhar a primeira janela
+    glColor3f(0.4, 0.6, 0.8); // Cor azul claro
+    glPushMatrix();
+    glTranslatef(0.25, -0.1, 0.0); // Mover a janela para a direita e para baixo
+    glScalef(0.25, 0.25, 1.0); // Reduzir o tamanho da janela
+    glRectf(-0.5, -0.5, 0.5, 0.5);
+    glPopMatrix();
+    
+    // Desenhar a segunda janela
+    glColor3f(0.4, 0.6, 0.8); // Cor azul claro
+    glPushMatrix();
+    glTranslatef(0.25, 0.1, 0.0); // Mover a janela para a direita e para cima
+    glScalef(0.25, 0.25, 1.0); // Reduzir o tamanho da janela
     glRectf(-0.5, -0.5, 0.5, 0.5);
     glPopMatrix();
     
