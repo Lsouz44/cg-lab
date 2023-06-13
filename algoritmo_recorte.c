930301da53d1bcb8bec357d57371bb12da9e80f9
Leandro Souza <48530574+Lsouz44@users.noreply.github.com>
2023-06-13 14:44:31 -0300

    Update algoritmo_recorte.c

--- algoritmo_recorte.c
+++ algoritmo_recorte.c
@@ -1,145 +1,160 @@
 #include <stdio.h>
-#include <GLFW/glfw3.h>
+#include <GL/glut.h>
 
-#define INSIDE 0 // Código para pontos dentro do retângulo de recorte
-#define LEFT 1   // Código para pontos à esquerda do retângulo de recorte
-#define RIGHT 2  // Código para pontos à direita do retângulo de recorte
-#define BOTTOM 4 // Código para pontos abaixo do retângulo de recorte
-#define TOP 8    // Código para pontos acima do retângulo de recorte
+// Definindo os códigos de recorte
+const int INSIDE = 0; // 0000
+const int LEFT = 1;   // 0001
+const int RIGHT = 2;  // 0010
+const int BOTTOM = 4; // 0100
+const int TOP = 8;    // 1000
 
-// Dimensões da janela
-const int WINDOW_WIDTH = 800;
-const int WINDOW_HEIGHT = 600;
+// Definindo a área de recorte retangular
+const float xmin = 50;
+const float ymin = 50;
+const float xmax = 200;
+const float ymax = 200;
 
-// Coordenadas do retângulo de recorte
-const int X_MIN = 50;
-const int Y_MIN = 50;
-const int X_MAX = 300;
-const int Y_MAX = 300;
-
-// Função para calcular o código de região de um ponto
-int calculateCode(int x, int y) {
-    int code = INSIDE;
-
-    if (x < X_MIN)      // Verificar se o ponto está à esquerda do retângulo
-        code |= LEFT;
-    else if (x > X_MAX) // Verificar se o ponto está à direita do retângulo
-        code |= RIGHT;
-
-    if (y < Y_MIN)      // Verificar se o ponto está abaixo do retângulo
-        code |= BOTTOM;
-    else if (y > Y_MAX) // Verificar se o ponto está acima do retângulo
-        code |= TOP;
-
-    return code;
-}
-
-// Função para realizar o recorte de uma linha utilizando o algoritmo de Cohen-Sutherland
-void cohenSutherland(int x1, int y1, int x2, int y2) {
-    int code1 = calculateCode(x1, y1);
-    int code2 = calculateCode(x2, y2);
-    int accept = 0;
-    int done = 0;
-
-    while (!done) {
-        if ((code1 == 0) && (code2 == 0)) { // Linha completamente dentro do retângulo
-            accept = 1;
-            done = 1;
-        } else if (code1 & code2) {         // Linha completamente fora do retângulo
-            done = 1;
-        } else {
-            int x, y;
-            int code = code1 ? code1 : code2;
-
-            if (code & TOP) {                // Ponto acima do retângulo
-                x = x1 + (x2 - x1) * (Y_MAX - y1) / (y2 - y1);
-                y = Y_MAX;
-            } else if (code & BOTTOM) {      // Ponto abaixo do retângulo
-                x = x1 + (x2 - x1) * (Y_MIN - y1) / (y2 - y1);
-                y = Y_MIN;
-            } else if (code & RIGHT) {       // Ponto à direita do retângulo
-                y = y1 + (y2 - y1) * (X_MAX - x1) / (x2 - x1);
-                x = X_MAX;
-            } else if (code & LEFT) {        // Ponto à esquerda do retângulo
-                y = y1 + (y2 - y1) * (X_MIN - x1) / (x2 - x1);
-                x = X_MIN;
-            }
-
-            if (code == code1) {
-                x1 = x;
-                y1 = y;
-                code1 = calculateCode(x1, y1);
-            } else {
-                x2 = x;
-                y2 = y;
-                code2 = calculateCode(x2, y2);
-            }
-        }
-    }
-
-    if (accept) {
-        glBegin(GL_LINES);
-        glColor3f(0.0f, 1.0f, 0.0f);  // Linha verde para a linha recortada
-        glVertex2i(x1, y1);
-        glVertex2i(x2, y2);
-        glEnd();
-    } else {
-        glBegin(GL_LINES);
-        glColor3f(1.0f, 0.0f, 0.0f);  // Linha vermelha para a linha completamente fora do retângulo
-        glVertex2i(x1, y1);
-        glVertex2i(x2, y2);
-        glEnd();
-    }
-}
-
-void drawRectangle() {
-    glBegin(GL_LINE_LOOP);
-    glColor3f(0.0f, 0.0f, 1.0f);  // Retângulo azul
-    glVertex2i(X_MIN, Y_MIN);
-    glVertex2i(X_MAX, Y_MIN);
-    glVertex2i(X_MAX, Y_MAX);
-    glVertex2i(X_MIN, Y_MAX);
+// Função para desenhar uma linha usando OpenGL
+void drawLine(float x1, float y1, float x2, float y2, float r, float g, float b)
+{
+    glColor3f(r, g, b);
+    glBegin(GL_LINES);
+    glVertex2f(x1, y1);
+    glVertex2f(x2, y2);
     glEnd();
 }
 
-void renderScene(GLFWwindow* window) {
-    glClear(GL_COLOR_BUFFER_BIT);
-    drawRectangle();
-    cohenSutherland(20, 60, 200, 400); // Exemplo de linha a ser recortada
-    cohenSutherland(0, 0, 49, 49);
-    glfwSwapBuffers(window);
+// Função para realizar o recorte de Cohen-Sutherland
+void clipLine(float x1, float y1, float x2, float y2)
+{
+    float x, y;
+    int code1, code2, codeOut;
+
+    // Determinar o código de recorte para o primeiro ponto
+    code1 = (x1 < xmin) ? LEFT : ((x1 > xmax) ? RIGHT : INSIDE);
+    code1 |= (y1 < ymin) ? BOTTOM : ((y1 > ymax) ? TOP : INSIDE);
+
+    // Determinar o código de recorte para o segundo ponto
+    code2 = (x2 < xmin) ? LEFT : ((x2 > xmax) ? RIGHT : INSIDE);
+    code2 |= (y2 < ymin) ? BOTTOM : ((y2 > ymax) ? TOP : INSIDE);
+
+    // Realizar o recorte
+    while (code1 != INSIDE || code2 != INSIDE)
+    {
+        // Verificar se a linha está completamente fora da área de recorte
+        if ((code1 & code2) != INSIDE)
+        {
+            return;
+        }
+
+        // Escolher um ponto fora da área de recorte
+        codeOut = code1 != INSIDE ? code1 : code2;
+
+        // Calcular a interseção com as arestas da área de recorte
+        if (codeOut & TOP)
+        {
+            x = x1 + (x2 - x1) * (ymax - y1) / (y2 - y1);
+            y = ymax;
+        }
+        else if (codeOut & BOTTOM)
+        {
+            x = x1 + (x2 - x1) * (ymin - y1) / (y2 - y1);
+            y = ymin;
+        }
+        else if (codeOut & RIGHT)
+        {
+            y = y1 + (y2 - y1) * (xmax - x1) / (x2 - x1);
+            x = xmax;
+        }
+        else if (codeOut & LEFT)
+        {
+            y = y1 + (y2 - y1) * (xmin - x1) / (x2 - x1);
+            x = xmin;
+        }
+
+        // Atualizar o ponto fora da área de recorte
+        if (codeOut == code1)
+        {
+            x1 = x;
+            y1 = y;
+            code1 = (x1 < xmin) ? LEFT : ((x1 > xmax) ? RIGHT : INSIDE);
+            code1 |= (y1 < ymin) ? BOTTOM : ((y1 > ymax) ? TOP : INSIDE);
+        }
+        else
+        {
+            x2 = x;
+            y2 = y;
+            code2 = (x2 < xmin) ? LEFT : ((x2 > xmax) ? RIGHT : INSIDE);
+            code2 |= (y2 < ymin) ? BOTTOM : ((y2 > ymax) ? TOP : INSIDE);
+        }
+    }
+
+    // Desenhar a linha recortada
+    drawLine(x1, y1, x2, y2, 0.0f, 1.0f, 0.0f);
 }
 
-int main() {
-    GLFWwindow* window;
+// Função para exibir os resultados na tela usando OpenGL
+void display()
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    
+    // Desenhar a área de recorte retangular em preto
+    drawLine(xmin, ymin, xmin, ymax, 0.0f, 0.0f, 0.0f);
+    drawLine(xmin, ymax, xmax, ymax, 0.0f, 0.0f, 0.0f);
+    drawLine(xmax, ymax, xmax, ymin, 0.0f, 0.0f, 0.0f);
+    drawLine(xmax, ymin, xmin, ymin, 0.0f, 0.0f, 0.0f);
 
-    if (!glfwInit()) {
-        printf("Falha ao inicializar GLFW\n");
-        return -1;
-    }
+    // Desenhar as linhas de teste
+    drawLine(100, 80, 150, 120, 1.0f, 0.0f, 0.0f);
+    drawLine(80, 160, 220, 190, 1.0f, 0.0f, 0.0f);
+    drawLine(50, 130, 250, 130, 1.0f, 0.0f, 0.0f);
+    drawLine(170, 220, 170, 50, 1.0f, 0.0f, 0.0f);
+    drawLine(120, 70, 220, 230, 1.0f, 0.0f, 0.0f);
+    drawLine(180, 240, 240, 40, 1.0f, 0.0f, 0.0f);
+    drawLine(60, 190, 200, 50, 1.0f, 0.0f, 0.0f);
+    drawLine(160, 30, 210, 250, 1.0f, 0.0f, 0.0f);
+    drawLine(90, 210, 250, 80, 1.0f, 0.0f, 0.0f);
+    drawLine(30, 30, 30, 180, 1.0f, 0.0f, 0.0f);
 
-    window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Recorte de Cohen-Sutherland", NULL, NULL);
-    if (!window) {
-        printf("Falha ao criar janela GLFW\n");
-        glfwTerminate();
-        return -1;
-    }
+    // Realizar o recorte de Cohen-Sutherland para cada linha de teste
+    clipLine(100, 80, 150, 120);
+    clipLine(80, 160, 220, 190);
+    clipLine(50, 130, 250, 130);
+    clipLine(170, 220, 170, 50);
+    clipLine(120, 70, 220, 230);
+    clipLine(180, 240, 240, 40);
+    clipLine(60, 190, 200, 50);
+    clipLine(160, 30, 210, 250);
+    clipLine(90, 210, 250, 80);
+    clipLine(30, 30, 30, 180);
 
-    glfwMakeContextCurrent(window);
-    glOrtho(0, WINDOW_WIDTH, WINDOW_HEIGHT, 0, -1, 1);
+    glFlush();
+}
 
-    while (!glfwWindowShouldClose(window)) {
-        renderScene(window);
-        glfwPollEvents();
-    }
+// Função de inicialização do OpenGL
+void init()
+{
+    glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluOrtho2D(0.0, 300.0, 0.0, 300.0);
+}
 
-    glfwTerminate();
+int main(int argc, char** argv)
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(800, 600);
+    glutCreateWindow("Recorte de Cohen-Sutherland");
+    init();
+    glutDisplayFunc(display);
+    glutMainLoop();
     return 0;
 }
 
 
 // para compilar:
-// g++ algoritmo_recorte.c -o algoritmo_recorte -lGL -lGLU -lglut -lm -lGLEW -lglfw3 -lX11
+// g++ algoritmo_recorte.c -o algoritmo_recorte -lGL -lGLU -lglut -lm
 
 // para executar:
 // ./algoritmo_recorte
